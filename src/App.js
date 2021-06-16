eccd4c0d84883ae85da7db050e1461811e6e944e
nitishsai9 <kommaraju.nitish.9@gmail.com>
2021-06-16 13:07:30 +0530

    Fav places to watch For

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useEffect, useState } from 'react';
+import Loading from './loading';
+const url = 'https://course-api.com/react-tours-project'
 function App() {
+  const [loading,setLoading]=useState(true);
+  const [places,setPlaces]=useState([]);
+  const [showMore,setshowMore]=useState(false);
+  const fetchData = async ()=>{
+    setLoading(true);
+    try{
+    const response = await fetch(url);
+    const data = await response.json();
+    setPlaces(data);
+    console.log(data)
+    setLoading(false);
+    return;
+    }
+    catch(err){
+      setLoading(false);
+      console.log(err);
+    }
+
+  };
+
+ 
+  useEffect(()=>{
+ 
+    fetchData();
+  },[]);
+
+  const OnHandler=(id)=>{
+   let newP= places.filter((mp)=> mp.id !== id);
+   setPlaces(newP);
+
+  }
+  if(loading){
+return (<div>
+  <Loading />
+</div>
+);
+  }
+  if(places.length===0){
+    return (
+      <div>
+        <button onClick={(e)=>{fetchData()}}>Refresh</button>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {
+     places.map((mp)=>{
+       const {id,name,info,image,price}= mp;
+       return <div key={id} style={{padding: "10px"}}>
+         <img src={image} height="100" width="100" />
+         <h1>{name}  ${price} </h1>
+         <p>
+           
+          {showMore ? info :`${info.substring(0,100)}` }
+
+           <button onClick={(e)=>{setshowMore(!showMore)}}
+   
+          >
+            {showMore ? "ShowMore" : "ShowLess"}
+
+          </button>
+         </p>
+         <button onClick={()=>OnHandler(id)} >Not Intrested</button>
+       </div>;
+     })
+    }
+    </>
   );
 }
 
